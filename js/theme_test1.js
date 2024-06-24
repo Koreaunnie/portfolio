43f03cc45a500150008cd349ff0013138cde7fd8
koreaunnie <hyunjung1995@gmail.com>
2024-06-24 11:33:20 +0900

    06/24/2024 revised

---
+++ js/theme_test1.js
@@ -0,0 +1,68 @@
+$(function(){
+	
+    // header
+	$('#fullpage').fullpage({
+		licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+		autoScrolling: true,
+		verticalCentered: true,
+		scrollingSpeed: 700,   // 기본값 700
+		navigation: true,
+		navigationPosition: 'right'
+	});
+		
+
+
+	// 다크 모드
+	const $checkbox = document.querySelector('#theme_handler');
+
+	const isUserColorTheme = localStorage.getItem('color-theme');
+	const isOsColorTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
+	const getUserTheme = () => (isUserColorTheme ? isUserColorTheme : isOsColorTheme);
+
+	window.onload = function () {
+	if (getUserTheme === 'light') {
+		localStorage.setItem('color-theme', 'light');
+		document.documentElement.setAttribute('color-theme', 'light');
+		$checkbox.setAttribute('checked', true);
+	} else {
+		localStorage.setItem('color-theme', 'dark');
+		document.documentElement.setAttribute('color-theme', 'dark');
+	}
+	};
+
+	$checkbox.addEventListener('click', e => {
+	if (e.target.checked) {
+		localStorage.setItem('color-theme', 'dark');
+		document.documentElement.setAttribute('color-theme', 'light');
+	} else {
+		localStorage.setItem('color-theme', 'dark');
+		document.documentElement.setAttribute('color-theme', 'dark');
+	}
+	});
+	
+
+	// 다크 모드 사용자 선택을 로컬 정보로 저장하기
+	document.getElementById('toggle').addEventListener("click", ()=>{ //다크모드 토글 클릭 발생
+		let colorMode = localStorage.getItem("colorMode");
+	
+		if(colorMode == 'dark'){ // 현재 값이 다크모드이면
+			localStorage.setItem("colorMode", "light");
+			document.body.classList.remove("dark-mode");
+		}else{
+			localStorage.setItem("colorMode", "dark");
+			document.body.classList.add("dark-mode");
+		}
+	},false);
+
+
+	// 주/야간 시간대에 따라 다크 모드 자동 전환하기
+	const hour = +new Date().toLocaleTimeString([],{ hour: '2-digit', hour12: false });//24시간 시간 값만 얻음
+		if (7 <= hour && hour < 18) { // 오전 7이 이상, 저녁 6시 미만
+			document.body.classList.add("light-mode");
+		} else {
+			document.body.classList.add("dark-mode");
+		}
+	
+
+});
